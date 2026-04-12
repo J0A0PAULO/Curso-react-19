c070f31518833c8bcec772ea74dde91b0d0bc570
João <moreirajoaopaulo951@gmail.com>
2026-04-11 23:43:24 -0300

    criando primeiro component

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+export function App() {
+  console.log();
+  return (
+    <>
+      <div>
+        <h1>ola mundo</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ex, ullam quo a corporis aperiam dignissimos quod iure odio tenetur culpa quibusdam maxime laborum sequi qui nulla quas odit ipsa.</p>
+      </div>
+    </>
+  )
+}
