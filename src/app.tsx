6502360f30293de458033b2d7f36f4e8a55b9b63
João <moreirajoaopaulo951@gmail.com>
2026-04-24 16:51:07 -0300

    utilizando useEffect

--- src/app.tsx
+++ src/app.tsx
@@ -1,28 +1,20 @@
 import { PlayCircleIcon } from 'lucide-react';
-import { useState } from 'react';
 import { Container } from './components/container/Container';
 import { CountDown } from './components/countDown/CountDown';
 import { Cycles } from './components/cycles/Cycles';
 import { DefaultButton } from './components/defaultButton/defaultButton';
 import { DefaultInput } from './components/defaultInput/DefaultInput';
 import { Footer } from './components/footer/Footer';
-import { Heading } from './components/heading/Heading';
+
 import { Logo } from './components/logo/Logo';
 import { Menu } from './components/menu/Menu';
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
-  const [numero, setNumero] = useState(0);
 
   return (
     <>
-      <Heading>Numero : {numero}</Heading>
-      <button onClick={() => {
-        console.log(setNumero(numero + 1), Date.now().toString().slice(-4, -1));
-
-      }}>Aumenta</button>
-
       <Container>
         <Logo></Logo>
       </Container>
@@ -35,7 +27,7 @@ export function App() {
       <Container>
         <form action="" className='form'>
           <div className="formRow">
-            <DefaultInput labelText={numero.toString()} id='meuId' type='text' placeholder='Digite Algo'></DefaultInput>
+            <DefaultInput labelText='task' id='meuId' type='text' placeholder='Digite Algo'></DefaultInput>
           </div>
           <div className="formRow">
             <p>Lorem ipsum dolor sit amet.</p>
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,20 +1,56 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import { useEffect, useState } from "react";
 import styles from './Menu.module.css';
 
+type AvailableThemes = 'dark' | 'light';
+
 export function Menu() {
+
+  const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+  function handleThemeChange(evento: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    evento.preventDefault();
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+
+      return nextTheme;
+    })
+    // document.documentElement.setAttribute('data-theme', theme);
+  }
+
+  // useEffect(() => {
+  //   console.log('useEffect sem depedencia', Date.now())
+  // });
+
+  // useEffect(() => {
+  //   console.log('useEffect com array deps vazia', Date.now())
+  // }, []);
+
+  useEffect(() => {
+    console.log('theme mudou dark', Date.now())
+    document.documentElement.setAttribute('data-theme', theme);
+
+    return () => {
+      console.log('olha este componente sera atualizado');
+    };
+  }, [theme]); // Executa  apenas quando valor de theme muda
+
+
   return (
     <>
       <nav className={styles.Menu}>
-        <a href="#" className={styles.MenuLink}>
+        <h1>{theme}</h1>
+        <a href="#" className={styles.MenuLink} aria-label="Ir para a Home" title="Ir para a Home">
           <HouseIcon></HouseIcon>
         </a>
-        <a href="#" className={styles.MenuLink}>
+        <a href="#" className={styles.MenuLink} aria-label="Ver histórico" title="Ver histórico">
           <HistoryIcon></HistoryIcon>
         </a>
-        <a href="#" className={styles.MenuLink}>
+        <a href="#" className={styles.MenuLink} aria-label="Configuracoes" title="Configuracoes">
           <SettingsIcon></SettingsIcon>
         </a>
-        <a href="#" className={styles.MenuLink}>
+        <a href="http://google.com" onClick={handleThemeChange} className={styles.MenuLink} aria-label="Mudar Tema" title="Mudar Tema">
           <SunIcon></SunIcon>
         </a>
       </nav>
